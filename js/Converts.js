644bd8b503e78f40d1d3099e263f5dcefceac028
slacker_HD <hudi@linuxmail.org>
2018-01-10 13:45:44 +0800

    add export funs

---
+++ js/Converts.js
@@ -0,0 +1,105 @@
+function init() {
+    document.getElementById("directory").innerHTML = GetWorkDir();
+}
+
+function GetWorkDir() {
+    if (!pfcIsWindows())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    var session = pfcGetProESession();
+    return session.GetCurrentDirectory();
+}
+
+/// creo脑残，dirty work around here
+function getrealFilename(filename) {
+    var reg = /\.[^\.]+$/.exec(filename);
+    if (reg != ".prt") {
+        filename = filename.slice(0, filename.length - reg.toString().length)
+        var arr = filename.split('\\');
+        return arr[arr.length - 1];
+    }
+}
+
+function ExportSteps() {
+    if (!pfcIsWindows())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    var session = pfcGetProESession();
+    var files = session.ListFiles("*.prt", pfcCreate("pfcFileListOpt").FILE_LIST_LATEST, GetWorkDir());
+    for (var i = 0; i < files.Count; i++) {
+        var mdlDescr = pfcCreate("pfcModelDescriptor").CreateFromFileName(getrealFilename(files.Item(i)));
+        var mdl = session.GetModelFromDescr(mdlDescr);
+        if (mdl == void null) {
+            mdl = session.RetrieveModel(mdlDescr);
+        }
+        var inConfiguration = pfcCreate("pfcAssemblyConfiguration");
+        var inGeometry = pfcCreate("pfcGeometryFlags").Create();
+        inGeometry.AsSolids = true;
+        var STEP3DExportInstructions = pfcCreate("pfcSTEP3DExportInstructions").Create(inConfiguration.EXPORT_ASM_SINGLE_FILE, inGeometry);
+        mdl.Export(getrealFilename(files.Item(i)) + ".stp", STEP3DExportInstructions);
+    }
+    session.EraseUndisplayedModels();
+    session.UIShowMessageDialog("文件已全部导出。", null);
+}
+
+function ExportIgeses() {
+    if (!pfcIsWindows())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    var session = pfcGetProESession();
+    var files = session.ListFiles("*.prt", pfcCreate("pfcFileListOpt").FILE_LIST_LATEST, GetWorkDir());
+    for (var i = 0; i < files.Count; i++) {
+        var mdlDescr = pfcCreate("pfcModelDescriptor").CreateFromFileName(getrealFilename(files.Item(i)));
+        var mdl = session.GetModelFromDescr(mdlDescr);
+        if (mdl == void null) {
+            mdl = session.RetrieveModel(mdlDescr);
+        }
+        var GeomExportFlags = pfcCreate("pfcGeomExportFlags").Create();
+        GeomExportFlags.Bezier = true;
+        GeomExportFlags.ExtendSRF = false;
+        var IGES3DExportInstructions = pfcCreate("pfcIGES3DExportInstructions").Create(GeomExportFlags);
+        mdl.Export(getrealFilename(files.Item(i)) + ".igs", IGES3DExportInstructions);
+    }
+    session.EraseUndisplayedModels();
+    session.UIShowMessageDialog("文件已全部导出。", null);
+}
+
+function ExportPdfs() {
+    if (!pfcIsWindows())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    var session = pfcGetProESession();
+    var files = session.ListFiles("*.drw", pfcCreate("pfcFileListOpt").FILE_LIST_LATEST, GetWorkDir());
+    for (var i = 0; i < files.Count; i++) {
+        var mdlDescr = pfcCreate("pfcModelDescriptor").CreateFromFileName(getrealFilename(files.Item(i)));
+        var mdl = session.GetModelFromDescr(mdlDescr);
+        if (mdl == void null) {
+            mdl = session.RetrieveModel(mdlDescr);
+        }
+        var PDFExportInstructions = pfcCreate("pfcPDFExportInstructions").Create();
+        PDFExportInstructions.Options = null;
+        PDFExportInstructions.ProfilePath = null;
+        PDFExportInstructions.FilePath = getrealFilename(files.Item(i)) + ".pdf";
+        alert(getrealFilename(files.Item(i)) + ".pdf")
+        mdl.Display();
+        mdl.Export(getrealFilename(files.Item(i)) + ".pdf", PDFExportInstructions);
+        mdl.Erase();
+    }
+    session.EraseUndisplayedModels();
+    session.UIShowMessageDialog("文件已全部导出。", null);
+}
+
+
+function ExportDwgs() {
+    if (!pfcIsWindows())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    var session = pfcGetProESession();
+    var files = session.ListFiles("*.drw", pfcCreate("pfcFileListOpt").FILE_LIST_LATEST, GetWorkDir());
+    for (var i = 0; i < files.Count; i++) {
+        var mdlDescr = pfcCreate("pfcModelDescriptor").CreateFromFileName(getrealFilename(files.Item(i)));
+        var mdl = session.GetModelFromDescr(mdlDescr);
+        if (mdl == void null) {
+            mdl = session.RetrieveModel(mdlDescr);
+        }
+        var DWG3DExportInstructions = pfcCreate("pfcDWG3DExportInstructions").Create();
+        mdl.Export(getrealFilename(files.Item(i)) + ".dwg", DWG3DExportInstructions);
+    }
+    session.EraseUndisplayedModels();
+    session.UIShowMessageDialog("文件已全部导出。", null);
+}
--- js/pfcUtils.js
+++ js/pfcUtils.js
@@ -6,95 +6,81 @@
 01-MAY-07   L-01-31   $$3   JCN      New exception messaging.
  */
 
-function isProEEmbeddedBrowser ()
-{
-  if (top.external && top.external.ptc)
-    return true;
-  else
-    return false;
+function isProEEmbeddedBrowser() {
+    if (top.external && top.external.ptc)
+        return true;
+    else
+        return false;
 }
 
-function pfcIsWindows ()
-{
-  if (navigator.appName.indexOf ("Microsoft") != -1)
-    return true;
-  else
-    return false;
+function pfcIsWindows() {
+    if (navigator.appName.indexOf("Microsoft") != -1)
+        return true;
+    else
+        return false;
 }
 
-function pfcCreate (className)
-{
-  if (!pfcIsWindows())	
-    netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-  
-  if (pfcIsWindows())
-    return new ActiveXObject ("pfc."+className);
-  else
-    {
-      ret = Components.classes ["@ptc.com/pfc/" + className + ";1"].
-	
-	createInstance();
-      
-      return ret;
+function pfcCreate(className) {
+    if (!pfcIsWindows())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+
+    if (pfcIsWindows())
+        return new ActiveXObject("pfc." + className);
+    else {
+        ret = Components.classes["@ptc.com/pfc/" + className + ";1"].
+
+        createInstance();
+
+        return ret;
     }
 }
 
-function pfcGetProESession ()
-{
-  if (!isProEEmbeddedBrowser ())
-    {
-      throw new Error ("Not in embedded browser.  Aborting...");
+function pfcGetProESession() {
+    if (!isProEEmbeddedBrowser()) {
+        throw new Error("Not in embedded browser.  Aborting...");
     }
-  
-  // Security code
-  if (!pfcIsWindows())
-    netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-  
-  var glob = pfcCreate ("MpfcCOMGlobal");
-  return glob.GetProESession();
+
+    // Security code
+    if (!pfcIsWindows())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+
+    var glob = pfcCreate("MpfcCOMGlobal");
+    return glob.GetProESession();
 }
 
-function pfcGetScript ()
-{  
-  if (!isProEEmbeddedBrowser ())
-    {
-      throw new Error ("Not in embedded browser.  Aborting...");
+function pfcGetScript() {
+    if (!isProEEmbeddedBrowser()) {
+        throw new Error("Not in embedded browser.  Aborting...");
     }
-  
-  // Security code
-  if (!pfcIsWindows())
-    netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-  
-  var glob = pfcCreate ("MpfcCOMGlobal");
-  return glob.GetScript();
+
+    // Security code
+    if (!pfcIsWindows())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+
+    var glob = pfcCreate("MpfcCOMGlobal");
+    return glob.GetScript();
 }
 
 
-function pfcGetExceptionDescription (err)
-{
- if (pfcIsWindows())
-    errString = err.description;
- else
-      errString = err.message;
+function pfcGetExceptionDescription(err) {
+    if (pfcIsWindows())
+        errString = err.description;
+    else
+        errString = err.message;
 
- return errString;
+    return errString;
 }
 
-function pfcGetExceptionType (err)
-{
-  errString = pfcGetExceptionDescription (err);
+function pfcGetExceptionType(err) {
+    errString = pfcGetExceptionDescription(err);
 
-  // This should remove the XPCOM prefix ("XPCR_C")
-  if (errString.search ("XPCR_C") < 0)
-  {
-	errString = errString.replace ("Exceptions::", "");
-	semicolonIndex = errString.search (";");
-	if (semicolonIndex > 0)
-		errString = errString.substring (0, semicolonIndex);
-	return (errString);
-  }
-  else
-      return (errString.replace("XPCR_C", ""));
+    // This should remove the XPCOM prefix ("XPCR_C")
+    if (errString.search("XPCR_C") < 0) {
+        errString = errString.replace("Exceptions::", "");
+        semicolonIndex = errString.search(";");
+        if (semicolonIndex > 0)
+            errString = errString.substring(0, semicolonIndex);
+        return (errString);
+    } else
+        return (errString.replace("XPCR_C", ""));
 }
-
-      
