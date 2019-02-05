0160febedaac6ee903d9bd25818f17d1e2bc8e4a
slacker_HD <hudi@linuxmail.org>
2019-02-05 19:57:34 +0800

    change NavigatorPaneBrowserAdd

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function init() {
-    // if (!pfcIsWindows())
-    //     netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    if (!pfcIsWindows())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
     // var session = pfcGetProESession();
-    // session.NavigatorPaneBrowserAdd("辅助设计", null, "file:///D:/mydoc/creo_weblink/index.html");
+    // session.NavigatorPaneBrowserAdd("辅助设计", null, session.CurrentWindow.GetURL());
 }
