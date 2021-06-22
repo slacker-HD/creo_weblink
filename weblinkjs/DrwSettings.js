4031816ce8cac5f49bfafab8487ed95d2f5b82c5
slacker-HD <hudi@linuxmail.org>
2021-06-22 19:30:24 +0800

    完成批量修改工程图绘图设置选项

--- weblinkjs/DrwSettings.js
+++ weblinkjs/DrwSettings.js
@@ -3,6 +3,7 @@ function init() {
     AddListView(".drw");
 }
 
+var dtlfile = "";
 function GetWorkDir() {
     if (pfcIsMozilla())
         netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
@@ -17,6 +18,7 @@ function selectdtl() {
     try {
         var path = session.UIOpenFile(pfcCreate('pfcFileOpenOptions').Create("*.dtl"));
         document.getElementById("dtlfile").innerHTML = path;
+        dtlfile = path;
     } catch (error) {
 
     }
@@ -45,10 +47,39 @@ function AddListView(extension) {
         z = document.getElementById(extension).rows.length - 1;
         var tableRow = document.getElementById(extension).insertRow(z + 1);
         var Cell_0 = tableRow.insertCell(0);
-        Cell_0.innerHTML = '<input type="checkbox" checked="true" />';
+        Cell_0.innerHTML = document.getElementById(extension).rows.length;
         var Cell_1 = tableRow.insertCell(1);
-        Cell_1.innerHTML = document.getElementById(extension).rows.length;
+        Cell_1.innerHTML = files.Item(i);
         var Cell_2 = tableRow.insertCell(2);
-        Cell_2.innerHTML = getrealFilename(files.Item(i), extension);
+        Cell_2.innerHTML = '<input type="checkbox" checked="true" />';
     }
+}
+
+function DrwOptionMacroString(drwfile, dtlfile) {
+    if (pfcIsMozilla())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    drwfile = drwfile.split("\\").join("\\\\");
+    dtlfile = dtlfile.split("\\").join("\\\\");
+    alert(drwfile);
+    return ("~ Command `ProCmdModelOpen` ;~ Trail `UI Desktop` `UI Desktop` `DLG_PREVIEW_POST` `file_open`;~ Update `file_open` `Inputname` `" + drwfile + "`;~ Activate `file_open` `Inputname`;~ Select `main_dlg_cur` `appl_casc`;~ Activate `main_dlg_cur` `main_dlg_cur`;~ Close `main_dlg_cur` `appl_casc`;~ Command `ProCmdDwgProperties` ;~ Activate `mdlprops_dlg` `OPTIONS_lay_Controls.push_Change.lay_instance`;~ FocusOut `preferences` `InputOpt`;~ Activate `preferences` `Open`;~ Trail `UI Desktop` `UI Desktop` `DLG_PREVIEW_POST` `file_open`;~ Update `file_open` `Inputname` `" + dtlfile + "`;~ Activate `file_open` `Inputname`;~ Activate `preferences` `ApplySave`;~ FocusOut `preferences` `InputOpt`;~ Activate `preferences` `Close`;~ Activate `mdlprops_dlg` `DlgClose_push`;~ Command `ProCmdModelSave` ;~ Activate `file_saveas` `OK`;");
+}
+
+function MassReplace() {
+    if (pfcIsMozilla())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    var trs = document.getElementById(".drw").getElementsByTagName("tr");
+    var session = pfcGetProESession();
+    if (dtlfile === "") {
+        session.UIShowMessageDialog("请先选择dtl文件。", null);
+        return;
+    }
+    var macro = "IMI ";
+    session.UIShowMessageDialog("准备开始批量更换图框，受weblink功能限制，系统无法判断是否全部完成设置，请操作完成后手工确认。", null);
+    for (var i = 0; i < trs.length; i++) {
+        var tds = trs[i].getElementsByTagName("td");
+        if (tds[2].childNodes[0].checked === true) {
+            macro += DrwOptionMacroString(tds[1].innerText, dtlfile);
+        }
+    }
+    session.RunMacro(macro);
 }
