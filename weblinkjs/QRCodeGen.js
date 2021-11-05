cfe25f33a0aab1703157d5dd6d747dbd93a70ecf
slacker-HD <hudi@linuxmail.org>
2021-11-05 10:53:36 +0800

    QRCODE完成

--- weblinkjs/QRCodeGen.js
+++ weblinkjs/QRCodeGen.js
@@ -1,23 +1,25 @@
+var SYMNAME = "IMI_QRCODE";
+var DOTWIDTH = 1.0;
 function init() {
-    // document.getElementById("currentModel").innerHTML = GetCurrentModelName();
+    document.getElementById("currentModel").innerHTML = GetCurrentModelName();
 }
 
 function GetCurrentModelName() {
-    // if (pfcIsMozilla())
-    //     netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-    // var session = pfcGetProESession();
-    // if (session.CurrentModel == null)
-    //     return "--";
-    // return session.CurrentModel.FileName;
+    if (pfcIsMozilla())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    var session = pfcGetProESession();
+    if (session.CurrentModel == null)
+        return "--";
+    return session.CurrentModel.FileName;
 }
 
 function RefershCurrentWindow() {
-    // if (pfcIsMozilla())
-    //     netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-    // var session = pfcGetProESession();
-    // session.CurrentWindow.Refresh();
-    // session.CurrentWindow.Repaint();
-    // session.CurrentWindow.Activate();
+    if (pfcIsMozilla())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    var session = pfcGetProESession();
+    session.CurrentWindow.Refresh();
+    session.CurrentWindow.Repaint();
+    session.CurrentWindow.Activate();
 }
 
 function CurrentModel() {
@@ -27,85 +29,110 @@ function CurrentModel() {
     return session.CurrentModel;
 }
 
-function GenQRCode() {
-    document.getElementById("qrcode").innerHTML = "";
-    var qrcode = new QRCode(document.getElementById("qrcode"));
+function InsertQRCode() {
+    placeQRCode();
+}
 
-    qrcode.clear();
-    qrcode.makeCode(document.getElementById("qrcodetext").value);
-    var data = qrcode._oQRCode.modules;
-    for (var i = 0; i < data.length; i++) {
-        for (var j = 0; j < data[i].length; j++) {
-            // alert(data[i][j]);
+function CleanSymDef(SymName) {
+    if (pfcIsMozilla())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    var session = pfcGetProESession();
+    var drawing = session.CurrentModel;
+    var detailItems = drawing.ListDetailItems(pfcCreate("pfcDetailType").DETAIL_SYM_DEFINITION, null);
+    for (var i = 0; i < detailItems.Count; i++) {
+        var detailItem = detailItems.Item(i);
+        var detailSymbolDefInstructions = detailItem.GetInstructions();
+        if (detailSymbolDefInstructions.Name === SymName) {
+            detailItem.Delete();
+            break;
         }
     }
 }
 
+function placeSymInst(symDef) {
+    if (!pfcIsWindows())
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
 
-function InsertQRCode() {
-    // placeSymInst();
-    lineEntityCreate();
+    var session = pfcGetProESession();
+    var drawing = session.CurrentModel;
+
+    if (drawing.Type != pfcCreate("pfcModelType").MDL_DRAWING)
+        return;
+
+    var browserSize = session.CurrentWindow.GetBrowserSize();
+    session.CurrentWindow.SetBrowserSize(0.0);
+
+    var point = pfcCreate("pfcPoint3D");
+    while (true) {
+        var mouse =
+            session.UIGetNextMousePick(pfcCreate("pfcMouseButton").MouseButton_nil);
+
+        if (mouse.SelectedButton ==
+            pfcCreate("pfcMouseButton").MOUSE_BTN_LEFT) {
+            point = mouse.Position;
+            break;
+        }
+        else
+            return;
+    }
+
+    session.CurrentWindow.SetBrowserSize(browserSize);
+
+    var instrs = pfcCreate("pfcDetailSymbolInstInstructions").Create(symDef);
+    var position = pfcCreate("pfcFreeAttachment").Create(point);
+    var allAttachments = pfcCreate("pfcDetailLeaders").Create();
+    position.AttachmentPoint = point;
+    allAttachments.ItemAttachment = position;
+    instrs.InstAttachment = allAttachments;
+    var symInst = drawing.CreateDetailItem(instrs);
+    symInst.Show();
 }
 
-
-
-function lineEntityCreate() {
+function DrawQRCode(QRCodedetailItem) {
     if (pfcIsMozilla())
         netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
 
-    var color = pfcCreate("pfcStdColor").COLOR_QUILT;
     var session = pfcGetProESession();
+    var color = pfcCreate("pfcStdColor").COLOR_QUILT;
 
-    /*--------------------------------------------------------------------*\ 
-      Get the current drawing & its background view
-    \*--------------------------------------------------------------------*/
-    var drawing = session.CurrentModel;
+    document.getElementById("qrcode").innerHTML = "";
+    var qrcode = new QRCode(document.getElementById("qrcode"));
+    qrcode.clear();
+    qrcode.makeCode(document.getElementById("qrcodetext").value);
+    var data = qrcode._oQRCode.modules;
 
-    var currSheet = drawing.CurrentSheetNumber;
-    var view = drawing.GetSheetBackgroundView(currSheet);
+    var currentPosX = DOTWIDTH / 2;
+    var currentPosY = data.length * DOTWIDTH;
 
-    /*--------------------------------------------------------------------*\ 
-       Select the endpoints of the line
-    \*--------------------------------------------------------------------*/
-    session.CurrentWindow.SetBrowserSize(0.0);
+    for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < data[i].length; j++) {
+            if (data[i][j] == true) {
 
-    var left = pfcCreate("pfcMouseButton").MOUSE_BTN_LEFT;
-    var mouse1 = session.UIGetNextMousePick(left);
-    var start = mouse1.Position;
-    var mouse2 = session.UIGetNextMousePick(left);
-    var end = mouse2.Position;
+                var start = pfcCreate("pfcPoint3D");
+                start.Set(0, currentPosX);
+                start.Set(1, currentPosY);
+                start.Set(2, 0);
 
-    /*--------------------------------------------------------------------*\ 
-      Allocate and initialize a curve descriptor 
-    \*--------------------------------------------------------------------*/
-    var geom = pfcCreate("pfcLineDescriptor").Create(start, end);
+                var end = pfcCreate("pfcPoint3D");
+                end.Set(0, currentPosX);
+                end.Set(1, currentPosY - DOTWIDTH);
+                end.Set(2, 0.0);
 
-    /*--------------------------------------------------------------------*\ 
-      Allocate data for the draft entity 
-    \*--------------------------------------------------------------------*/
-    var instrs = pfcCreate("pfcDetailEntityInstructions").Create(geom,
-        view);
-
-    /*--------------------------------------------------------------------*\ 
-      Set the color to the specified Pro/ENGINEER predefined color 
-    \*--------------------------------------------------------------------*/
-    var rgb = session.GetRGBFromStdColor(color);
-    instrs.Color = rgb;
-
-    /*--------------------------------------------------------------------*\ 
-      Create the entity 
-    \*--------------------------------------------------------------------*/
-    drawing.CreateDetailItem(instrs);
-
-    /*--------------------------------------------------------------------*\ 
-      Display the entity 
-    \*--------------------------------------------------------------------*/
-    session.CurrentWindow.Repaint();
+                var geom = pfcCreate("pfcLineDescriptor").Create(start, end);
+                var instrs = pfcCreate("pfcDetailEntityInstructions").Create(geom, null);
+                instrs.width = DOTWIDTH;
+                var rgb = session.GetRGBFromStdColor(color);
+                instrs.Color = rgb;
+                QRCodedetailItem.CreateDetailItem(instrs);
+            }
+            currentPosX += DOTWIDTH;
+        }
+        currentPosX = DOTWIDTH / 2;
+        currentPosY -= DOTWIDTH;
+    }
 }
 
-
-
-function placeSymInst() {
+function placeQRCode() {
     if (pfcIsMozilla())
         netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
 
@@ -114,10 +141,12 @@ function placeSymInst() {
     if (drawing.Type != pfcCreate("pfcModelType").MDL_DRAWING)
         return;
 
-    var detailSymbolDefInstructions = pfcCreate("pfcDetailSymbolDefInstructions").Create("IMI_QRCODE");
-    detailSymbolDefInstructions.HasElbow = false;
-
-    var detailItem = drawing.CreateDetailItem(detailSymbolDefInstructions);
-    // pfcDetailSymbolDefItem.
-    // pfcDetailItem	CreateDetailItem (pfcDetailCreateInstructions Instructions)
+    CleanSymDef(SYMNAME);
+    var QRCodedetailItem;
+    var detailSymbolDefInstructions = pfcCreate("pfcDetailSymbolDefInstructions").Create(SYMNAME);
+    QRCodedetailItem = drawing.CreateDetailItem(detailSymbolDefInstructions);
+    DrawQRCode(QRCodedetailItem);
+    placeSymInst(QRCodedetailItem);
+    drawing.Regenerate();
+    RefershCurrentWindow();
 }
